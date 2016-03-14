7987c61c5b1f70c4294358832f1cb6f0eaff8c42
Josh Lindsay <jlindsay90@gmail.com>
2016-03-13 23:33:22 -0700

    make requests to get challenges and submissions. code organization.

--- main.py
+++ main.py
@@ -15,37 +15,25 @@
 import re
 import json
 import argparse
+import pickle
 import os.path
+import itertools
+import functools
+from sh import git
+from os import makedirs, chdir
 from requests import Request, Session
-from requests.utils import add_dict_to_cookiejar, dict_from_cookiejar
-from os import makedirs
-
 
 def getArgParser():
     parser = argparse.ArgumentParser(description='Free your HackerRank.com code!')
     parser.add_argument('-l', '--local', action='store', type=bool, metavar='local', help='local git repo')
     parser.add_argument('-r', '--remote', action='store', type=str, metavar='remote', help='remote git repo URL')
     parser.add_argument('-f', '--file', action='store', type=bool, metavar='file', help='parse HAR file')
-    parser.add_argument('-c', '--config', metavar='config')
     parser.add_argument('-d', '--daemon', action='store_true', default=False, help='run in background')
     parser.add_argument('-u', '--username', action='store', metavar='username', help='account username')
     parser.add_argument('-p', '--password', action='store', metavar='password', help='account password')
     return parser;
 
-def parseConfigFile(config):
-    # primarily get username and password from config file
-    # NOTE: ensure config file is included in .gitignore
-    pass
-
-def addCookies(s, cookies):
-    s.cookies = add_dict_to_cookiejar(s.cookies, cookies)
-
 def login(s, username, password):
-    payload = {
-        'login' : username,
-        'password' : password,
-        'remember_me' : 'false',
-    }
     csrf = None
     r = s.get('https://www.hackerrank.com/login')
     for line in r.text.split("\n"):
@@ -53,50 +41,64 @@ def login(s, username, password):
             csrf = re.sub(r"<meta content=\"([^\"]+)\".*", r"\1", line)
             break
     csrfDict = {'X-CSRF-TOKEN': csrf}
-    r = s.post('https://www.hackerrank.com/auth/login', data=payload, headers=csrfDict)
-    return r.status_code
+    payload = {
+        'login' : username,
+        'password' : password,
+        'remember_me' : 'false',
+    }
+    return s.post('https://www.hackerrank.com/auth/login', data=payload, headers=csrfDict)
 
-def getSubmissions(s, batchSize = -1):
-    # get total submission count
+def logout(s):
+    return s.delete('https://www.hackerrank.com/auth/logout')
+
+def getModelGenerator(s, batchSize = -1):
     r = s.get('https://www.hackerrank.com/rest/contests/master/submissions/grouped?limit=0')
-    total = r.json()['total']
     print(r.text)
+    total = r.json()['total']
     if batchSize <= 0:
         batchSize = total
 
     offset = 0
     while offset < total:
-        submissions = getSubmissionsByChallenge(s, offset, 10) # batchSize)
+        submissions = getSubmissionsByChallengeGrouped(s, offset, 3) #batchSize)
         offset += batchSize
-        challenges = getChallenges(submissions.keys())
-        submissions = getSubmissions(submissions.values())
-    return None
+        challenges = getChallenges(s, submissions.keys())
+        submissions = getSubmissions(s, functools.reduce(itertools.chain, submissions.values(), []))
+        yield (challenges, submissions)
 
-def getChallenges(challengeIds):
-    pass
-
-def getSubmissions(submissionIds):
-    pass
-
-def getSubmissionsByChallenge(s, offset, limit):
+def getSubmissionsByChallengeGrouped(s, offset, limit):
     params = {'offset':offset, 'limit':limit}
-    r = s.get('https://www.hackerrank.com/rest/contests/master/submissions/grouped', params = params)
+    r = s.get('https://www.hackerrank.com/rest/contests/master/submissions/grouped', params=params)
     submissions = {}
-    for subs in [model['submissions'] for model in r.json()["models"]]:
-        submissions[subs[0]["challenge_id"]] = [s['id'] for s in subs]
+    for subs in [m['submissions'] for m in r.json()["models"]]:
+        id = subs[0]["challenge_id"]
+        submissions[id] = [s['id'] for s in subs]
     return submissions
 
-def logout(s):
-    r = s.delete('https://www.hackerrank.com/auth/logout')
-    pass
+def getChallenges(s, challengeIds):
+    challenges = {}
+    for id in challengeIds:
+        r = s.get('https://www.hackerrank.com/rest/contests/master/challenges/' + str(id))
+        j = r.json()
+        challenges[id] = {}
+    print("Challenges")
+    print(challenges.keys())
+    return challenges
+
+def getSubmissions(s, submissionIds):
+    submissions = {}
+    for id in submissionIds:
+        r = s.get('https://www.hackerrank.com/rest/contests/master/submissions/' + str(id))
+        j = r.json()
+        submissions[id] = {}
+    print("Submissions")
+    print(submissions.keys())
+    return submissions
 
 def main():
-    # get config
+    # get args and validate
     parser = getArgParser()
     args = parser.parse_args()
-    if args.config:
-        args = parseConfigFile(args.config)
-
     if not args.username or not args.password:
         parser.print_help()
         exit(123)
@@ -106,15 +108,16 @@ def main():
     # do site interaction
     s = Session()
     login(s, args.username, args.password)
-    submissions = getSubmissions(s)
-    logout(s)
+    models = getModelGenerator(s)
 
-    if not submissions:
+    #makedirs("./hrdir")
+    #chdir("./hrdir")
+
+    for (challenges, submissions) in models:
         pass
 
-    # TODO build git repo
-    #if args.local:
-    #    makedirs(args.local)
-    #if args.remote:
+    # submissions will have to be sorted by time when written to git repo... uh oh
+
+    logout(s)
 
 main()
