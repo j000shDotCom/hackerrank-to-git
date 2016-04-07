368c2fe3afb506f7b0cae3915888b420416f04b4
Josh Lindsay <jlindsay90@gmail.com>
2016-04-07 09:43:59 -0700

    adding contest support

--- hackerrankops.py
+++ hackerrankops.py
@@ -1,12 +1,21 @@
 """
-  All HackerRank specific logic
+ All HackerRank specific logic
+
+ Relevant Links:
+ https://www.hackerrank.com/rest/contests/master/submissions/grouped?offset=0limit=2
+ https://www.hackerrank.com/rest/hackers/{username}/recent_challenges?offset=0&limit=2
+ https://www.hackerrank.com/rest/contests/master/challenges?limit=132352
+ https://www.hackerrank.com/rest/contests/master/challenges/{slug or challengeId}
+ https://www.hackerrank.com/rest/contests/master/submissions/{submissionId}
+ https://www.hackerrank.com/rest/contests/master/challenges/{slug}/submissions/?offset=0&limit=10
+ https://www.hackerrank.com/rest/hackers/jlindsay90/contest_participation?offset=5&limit=5
 """
 
 import re
 import os.path
 from itertools import chain
 
-def getHackerRankData(s, username, password, batchSize=-1):
+def getHackerRankData(s, username, password):
     print('LOGGING IN AS ' + username)
     data = {
         'login' : username,
@@ -16,7 +25,7 @@ def getHackerRankData(s, username, password, batchSize=-1):
     csrfHeader = {'X-CSRF-TOKEN': getCsrfToken(s)}
     s.post('https://www.hackerrank.com/auth/login', data=data, headers=csrfHeader)
     user = s.get("https://www.hackerrank.com/rest/contests/master/hackers/me").json()['model']
-    return {'models': getModelGenerator(s, batchSize, csrfHeader), 'user': user}
+    return {'models': getModelGenerator(s, csrfHeader), 'user': user}
 
 def getCsrfToken(s):
     r = s.get('https://www.hackerrank.com/')
@@ -24,24 +33,30 @@ def getCsrfToken(s):
         if ('csrf-token' in line):
             return re.sub(r"<meta content=\"([^\"]+)\".*", r"\1", line)
 
-def getModelGenerator(s, batchSize, csrfHeader):
-    r = s.get('https://www.hackerrank.com/rest/contests/master/submissions/grouped?limit=0')
-    total = r.json()['total']
-    if batchSize <= 0:
-        batchSize = total
-
+def getModelGenerator(s, csrfHeader, grouped):
+    # get submissions to master contest
+    url = 'https://www.hackerrank.com/rest/contests/master/submissions' + ('/grouped' if grouped else '')
+    total = s.get(url, params={limit:0}).json()['total']
     offset = 0
     while offset < total:
-        print('fetching batch [{}-{}] of {}'.format(offset, offset + batchSize, total))
-        ids = getSubmissionsByChallengeGrouped(s, offset, batchSize)
+        print('fetching batch [{}-{}] of {}'.format(offset, offset + 10, total))
+        ids = getSubmissionsByChallenge(s, offset, grouped)
         offset += batchSize
-        challenges = getChallenges(s, ids.keys())
-        submissions = getSubmissions(s, chain.from_iterable(ids.values()))
+        challenges = getModelsOfType(s, "challenges", ids.keys())
+        submissions = getModelsOfType(s, "submissions", chain.from_iterable(ids.values()))
         yield (ids, challenges, submissions)
-        break
 
     print('LOGGING OUT')
     s.delete('https://www.hackerrank.com/auth/logout', headers=csrfHeader)
+
+def getSubmissionsByContest(s, contest):
+    r = s.get('https://www.hackerrank.com/rest/hackers/jlindsay90/contest_participation')
+
+def getSubmissionsByChallenge(s, offset, limit):
+    params = {'offset':offset, 'limit':limit}
+    r = s.get('https://www.hackerrank.com/rest/contest/master/submissions', params=params)
+    submissions = []
+
 
 def getSubmissionsByChallengeGrouped(s, offset, limit):
     params = {'offset':offset, 'limit':limit}
@@ -52,25 +67,13 @@ def getSubmissionsByChallengeGrouped(s, offset, limit):
         ids[c_id] = [s['id'] for s in subs]
     return ids
 
-def getChallenges(s, challengeIds):
-    challenges = {}
-    for c_id in challengeIds:
-        print('  chal ' + str(c_id))
-        r = s.get('https://www.hackerrank.com/rest/contests/master/challenges/' + str(c_id))
+def getModelsOfType(s, modelType modelIds):
+    models = {}
+    for modelId in modelIds:
+        r = s.get('https://www.hackerrank.com/rest/contests/master/{}/{}'.format(modelType, modelId))
         if not r.text:
             print('    --RESPONSE IS EMPTY. SKIPPING.') #TODO
             continue
         challenges[c_id] = r.json()['model']
     return challenges
-
-def getSubmissions(s, submissionIds):
-    submissions = {}
-    for s_id in submissionIds:
-        print('  sub ' + str(s_id))
-        r = s.get('https://www.hackerrank.com/rest/contests/master/submissions/' + str(s_id))
-        if not r.text:
-            print('    --RESPONSE IS EMPTY. SKIPPING.') #TODO
-            continue
-        submissions[s_id] = r.json()['model']
-    return submissions
 
--- main.py
+++ main.py
@@ -3,14 +3,6 @@
  Joshua Lindsay (jlindsay90@gmail.com)
 
  Sign into HackerRank and archive solutions in a git repository (where they should be)
-
- Relevant Links:
- https://www.hackerrank.com/rest/contests/master/submissions/grouped?offset=0limit=2
- https://www.hackerrank.com/rest/hackers/{username}/recent_challenges?offset=0&limit=2
- https://www.hackerrank.com/rest/contests/master/challenges?limit=132352
- https://www.hackerrank.com/rest/contests/master/challenges/{slug or challengeId}
- https://www.hackerrank.com/rest/contests/master/submissions/{submissionId}
- https://www.hackerrank.com/rest/contests/master/challenges/{slug}/submissions/?offset=0&limit=10
 """
 import os.path
 from argparse import ArgumentParser
@@ -24,25 +16,14 @@ def getArgs():
     parser.add_argument('-p', '--password', action='store', metavar='password', required=True, help='account password')
     parser.add_argument('-f', '--file', action='store', metavar='file', help='create/use pickle file')
     parser.add_argument('-d', '--dir', action='store', metavar='dir', help='path repository path')
-    parser.add_argument('-b', '--batch', action='store', metavar='batch', type=int, help='challenge request batch size')
+    #parser.add_argument('-b', '--batch', action='store', metavar='batch', type=int, help='challenge request batch size')
     #parser.add_argument('-l', '--load', action='store_true', metavar='load', help='load pickle file')
     #parser.add_argument('-c', '--create', action='store_true', metavar='create', help='create pickle file')
     return parser.parse_args();
 
 def main():
     args = getArgs()
-
-    #if args.file and os.path.exists(args.file):
-    #    model = load(open(args.file, 'rb'))
-    #else:
-
-    s = Session()
-    data = getHackerRankData(s, args.username, args.password, args.batch)
-
-    # cannot pickle generator TODO
-    #if args.file and not os.path.exists(args.file):
-    #    dump(models, open(args.file, 'wb'))
-
+    data = getHackerRankData(Session(), args.username, args.password)
     archiveData(args.dir, data)
 
 main()
