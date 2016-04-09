d315374c4dba08549a4240a3db80dadf3befa42c
Josh Lindsay <jlindsay90@gmail.com>
2016-04-08 22:49:37 -0700

    procedural contest/challenge/submission collection WIP DNT

--- hackerrankops.py
+++ hackerrankops.py
@@ -16,7 +16,6 @@ import os.path
 from itertools import chain
 
 def getHackerRankData(s, username, password):
-    print('LOGGING IN AS ' + username)
     data = {
         'login' : username,
         'password' : password,
@@ -25,7 +24,9 @@ def getHackerRankData(s, username, password):
     csrfHeader = {'X-CSRF-TOKEN': getCsrfToken(s)}
     s.post('https://www.hackerrank.com/auth/login', data=data, headers=csrfHeader)
     user = s.get("https://www.hackerrank.com/rest/contests/master/hackers/me").json()['model']
-    return {'models': getModelGenerator(s, csrfHeader), 'user': user}
+    data = {'models': getModelGenerator(s, csrfHeader), 'user': user}
+    s.delete('https://www.hackerrank.com/auth/logout', headers=csrfHeader)
+    return data
 
 def getCsrfToken(s):
     r = s.get('https://www.hackerrank.com/')
@@ -33,30 +34,67 @@ def getCsrfToken(s):
         if ('csrf-token' in line):
             return re.sub(r"<meta content=\"([^\"]+)\".*", r"\1", line)
 
-def getModelGenerator(s, csrfHeader, grouped):
-    # get submissions to master contest
-    url = 'https://www.hackerrank.com/rest/contests/master/submissions' + ('/grouped' if grouped else '')
-    total = s.get(url, params={limit:0}).json()['total']
-    offset = 0
-    while offset < total:
-        print('fetching batch [{}-{}] of {}'.format(offset, offset + 10, total))
-        ids = getSubmissionsByChallenge(s, offset, grouped)
-        offset += batchSize
-        challenges = getModelsOfType(s, "challenges", ids.keys())
-        submissions = getModelsOfType(s, "submissions", chain.from_iterable(ids.values()))
-        yield (ids, challenges, submissions)
+def getModelGenerator(s, csrfHeader):
+    contests = {}
+    #url = 'https://www.hackerrank.com/rest/hackers/me/myrank_contests?limit=100&type=recent'
+    #contests = getAllModels(s, url2, ['master'])
+    url = 'https://www.hackerrank.com/rest/hackers/me/contest_participation'
+    contestSlugs = [m['slug'] for m in getAllModels(s, url, ['master'])]
+    for contestSlug in contestSlugs:
+        # get contest model
+        url = 'https://www.hackerrank.com/rest/contests/' + contest
+        contests[contestSlug] = s.get(url).json() # is {model:{}}
 
-    print('LOGGING OUT')
-    s.delete('https://www.hackerrank.com/auth/logout', headers=csrfHeader)
+        # NOTE only queries 5 submissions per challenge
+        #url = 'https://www.hackerrank.com/rest/contests/' + contest + '/submissions/grouped'
+        #group = getAllModels(s, url)
+        #groups += group
 
-def getSubmissionsByContest(s, contest):
-    r = s.get('https://www.hackerrank.com/rest/hackers/jlindsay90/contest_participation')
+        url = 'https://www.hackerrank.com/rest/contests/' + contest + '/submissions/'
+        submissions = getAllModels(s, url)
+        challenges = {s['challenge'] for s in submissions} # set comprehension ... COOL! <3 Python
 
-def getSubmissionsByChallenge(s, offset, limit):
+        # get challenge models
+        contests[contestSlug]['challenges'] = {}
+        for challenge in challenges:
+            url = 'https://www.hackerrank.com/rest/contests/' + contest + '/challenges/' + challenge['slug']
+            contests[contestSlug]['challenges'][chellenge['slug'] = s.get(url).json()['model']
+
+        # get submission models
+        contests[contestSlug]['submissions'] = {}
+        for submission in submissions:
+            url = 'https://www.hackerrank.com/rest/contests/' + contest + '/submissions/' + submission['id']
+            contests[contestSlug]['submissions'][submission['id'] = s.get(url).json()['model']
+
+        #yield contests[contestSlug]
+
+    return contests
+
+def getAllModels(s, url, lst = []):
+    r = s.get(url).json()
+    total = r['total']
+    lst += r['models']
+    if len(r['models']) < total:
+        lst += s.get(url, params={'offset'=len(r['models']), 'limit':total}).json()['models']
+    return lst
+
+def getModelsById(s, contest, modelType, modelIds, offset = -1, limit = -1):
+    modelsById = {}
+    for modelId in modelIds:
+        url = 'https://www.hackerrank.com/rest/contests/{}/{}/{}'.format(contest, modelType, modelId)
+        r = s.get(url).json()
+        models = r['models']
+        total = r['total']
+        if len(models) < total:
+            rest = s.get(url, params={'offset':len(models), 'limit':total}).json()['models']
+            models.extend(rest)
+    return models
+
+def getChallenges(s, contest, offset = -1, limit = -1):
+    url =
     params = {'offset':offset, 'limit':limit}
     r = s.get('https://www.hackerrank.com/rest/contest/master/submissions', params=params)
     submissions = []
-
 
 def getSubmissionsByChallengeGrouped(s, offset, limit):
     params = {'offset':offset, 'limit':limit}
@@ -66,14 +104,4 @@ def getSubmissionsByChallengeGrouped(s, offset, limit):
         c_id = subs[0]["challenge_id"]
         ids[c_id] = [s['id'] for s in subs]
     return ids
-
-def getModelsOfType(s, modelType modelIds):
-    models = {}
-    for modelId in modelIds:
-        r = s.get('https://www.hackerrank.com/rest/contests/master/{}/{}'.format(modelType, modelId))
-        if not r.text:
-            print('    --RESPONSE IS EMPTY. SKIPPING.') #TODO
-            continue
-        challenges[c_id] = r.json()['model']
-    return challenges
 
